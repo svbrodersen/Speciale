59172ea1d3a221d31d1e0fdf9207f04b40d2d1e6
Simon Vinding Brodersen <simon@3450.dk>
2026-03-04 15:10:04 +0100

    performance increase, reduce read from register calls
    non-needed not boolean

--- src/qemu_timing_plugin/src/utils/s3k.rs
+++ src/qemu_timing_plugin/src/utils/s3k.rs
@@ -4,40 +4,50 @@ use std::fmt::Write;
 
 include!(concat!(env!("OUT_DIR"), "/bindings_s3k.rs"));
 use std::{
-    collections::HashSet,
+    collections::HashMap,
     sync::{Mutex, OnceLock},
 };
 
 use crate::{
-    GByteArray, g_byte_array_new, g_byte_array_set_size,
+    g_byte_array_new, g_byte_array_set_size,
     qemu_plugin_hwaddr_operation_result_QEMU_PLUGIN_HWADDR_OPERATION_OK,
     qemu_plugin_read_memory_hwaddr, qemu_plugin_read_memory_vaddr, qemu_plugin_read_register,
-    qemu_plugin_register, utils::plugin_find_register,
+    qemu_plugin_register, utils::plugin_find_register, GByteArray,
 };
 
 use super::{DomainRetriever, SendPtr};
 
+struct VcpuState {
+    buf: SendPtr<GByteArray>,
+    last_tp: u64,
+    last_domain_id: Option<usize>,
+}
+
 pub struct S3KDomainRetriever {
     reg_tp: OnceLock<SendPtr<qemu_plugin_register>>,
-    cpu_buffers: Vec<Mutex<SendPtr<GByteArray>>>,
+    vcpu_states: Vec<Mutex<VcpuState>>,
+    tp_to_domain: Mutex<HashMap<u64, Option<usize>>>,
     proc_buf: Mutex<SendPtr<GByteArray>>,
-    unique_domain_tp: Mutex<HashSet<(usize, u64)>>,
 }
 
 impl DomainRetriever for S3KDomainRetriever {
     fn new(cores: usize) -> Self {
-        let mut cpu_buffers = Vec::with_capacity(cores);
+        let mut vcpu_states = Vec::with_capacity(cores);
 
         for _ in 0..cores {
             let buf = unsafe { g_byte_array_new() };
-            cpu_buffers.push(Mutex::new(SendPtr(buf)));
+            vcpu_states.push(Mutex::new(VcpuState {
+                buf: SendPtr(buf),
+                last_tp: u64::MAX,
+                last_domain_id: None,
+            }));
         }
 
         Self {
             reg_tp: OnceLock::new(),
-            cpu_buffers,
+            vcpu_states,
+            tp_to_domain: Mutex::new(HashMap::new()),
             proc_buf: Mutex::new(SendPtr(unsafe { g_byte_array_new() })),
-            unique_domain_tp: Mutex::new(HashSet::new()),
         }
     }
 
@@ -46,13 +56,7 @@ impl DomainRetriever for S3KDomainRetriever {
             .get_or_init(|| SendPtr(plugin_find_register("tp")));
     }
 
-    fn on_exit(&self, _out: &mut String) {
-        // writeln!(
-        //     out,
-        //     "Unique_domain_tp: {:?}",
-        //     self.unique_domain_tp.lock().unwrap()
-        // ).unwrap();
-    }
+    fn on_exit(&self, _out: &mut String) {}
 
     fn get_domain_id(&self, vcpu_index: u32) -> Option<usize> {
         let tp_handle = self.reg_tp.get()?.0;
@@ -60,28 +64,41 @@ impl DomainRetriever for S3KDomainRetriever {
             return None;
         }
 
-        let cache_idx = (vcpu_index as usize) % self.cpu_buffers.len();
-        let guard = self.cpu_buffers[cache_idx].lock().unwrap();
-        let buf_ptr = guard.0;
+        let cache_idx = (vcpu_index as usize) % self.vcpu_states.len();
+        let mut guard = self.vcpu_states[cache_idx].lock().unwrap();
+        let buf_ptr = guard.buf.0;
 
-        unsafe {
-            // Read tp
+        let tp_val = unsafe {
             g_byte_array_set_size(buf_ptr, 0);
             if !qemu_plugin_read_register(tp_handle, buf_ptr) {
                 return None;
             }
 
             let len = (*buf_ptr).len as usize;
-            let tp_val = match len {
+            match len {
                 4 => u64::from(u32::from_le_bytes(*((*buf_ptr).data as *const [u8; 4]))),
                 8 => u64::from_le_bytes(*((*buf_ptr).data as *const [u8; 8])),
                 _ => return None,
-            };
-
-            if tp_val == 0 {
-                return None;
             }
+        };
 
+        if tp_val == 0 {
+            return None;
+        }
+
+        if tp_val == guard.last_tp {
+            return guard.last_domain_id;
+        }
+
+        // Check global cache, in case we have already seen
+        if let Some(&domain_id) = self.tp_to_domain.lock().unwrap().get(&tp_val) {
+            guard.last_tp = tp_val;
+            guard.last_domain_id = domain_id;
+            return domain_id;
+        }
+
+        // Slow path: read memory
+        let domain_id = unsafe {
             let proc_size = std::mem::size_of::<proc_t>();
 
             let mutex_guard = self.proc_buf.lock().unwrap();
@@ -90,25 +107,27 @@ impl DomainRetriever for S3KDomainRetriever {
 
             let succ = qemu_plugin_read_memory_vaddr(tp_val, proc_buf, proc_size);
 
-            if !succ {
+            if succ {
+                let proc_ptr = (*proc_buf).data as *const proc_t;
+                let pid = std::ptr::read_unaligned(std::ptr::addr_of!((*proc_ptr).pid));
+                usize::try_from(pid).ok()
+            } else {
                 g_byte_array_set_size(proc_buf, proc_size as u32);
                 let succ_hw = qemu_plugin_read_memory_hwaddr(tp_val, proc_buf, proc_size);
 
                 if succ_hw != qemu_plugin_hwaddr_operation_result_QEMU_PLUGIN_HWADDR_OPERATION_OK {
-                    return None;
+                    None
+                } else {
+                    let proc_ptr = (*proc_buf).data as *const proc_t;
+                    let pid = std::ptr::read_unaligned(std::ptr::addr_of!((*proc_ptr).pid));
+                    usize::try_from(pid).ok()
                 }
             }
+        };
 
-            let proc_ptr = (*proc_buf).data as *const proc_t;
-            let pid = std::ptr::read_unaligned(std::ptr::addr_of!((*proc_ptr).pid));
-
-            if let Ok(domain_id) = usize::try_from(pid) {
-                let mut set = self.unique_domain_tp.lock().unwrap();
-                set.insert((domain_id, tp_val));
-                Some(domain_id)
-            } else {
-                None
-            }
-        }
+        self.tp_to_domain.lock().unwrap().insert(tp_val, domain_id);
+        guard.last_tp = tp_val;
+        guard.last_domain_id = domain_id;
+        domain_id
     }
 }
